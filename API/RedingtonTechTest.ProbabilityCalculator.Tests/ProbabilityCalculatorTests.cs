8fdcbbea7bfabd30b6cf62933b6d50c2d2683ba4
Alex Wilson <alex.wilson@bud.co.uk>
2019-08-10 13:02:00 +0100

    Added test cases for initial calculation

--- API/RedingtonTechTest.ProbabilityCalculator.Tests/ProbabilityCalculatorTests.cs
+++ API/RedingtonTechTest.ProbabilityCalculator.Tests/ProbabilityCalculatorTests.cs
@@ -13,7 +13,7 @@ namespace RedingtonTechTest.ProbabilityCalculator.Tests
         [TestCase(0.25, 0.25, 0.0625)]
         [TestCase(0.3, 0.9, 0.27)]
         [TestCase(0.3, 0.3, 0.09)]
-        public void CombineWith_should_return_the_product_of_two_probabilities(decimal A, decimal B, decimal expected)
+        public void CombineWith_should_return_the_product_of_the_inputs(decimal A, decimal B, decimal expected)
         {
             // arrange
             var sut = GetSubject();
@@ -24,6 +24,26 @@ namespace RedingtonTechTest.ProbabilityCalculator.Tests
             // assert
             actual.Should().Be(expected);
         }
+
+        [TestCase(0.5, 0.5, 0.75)]
+        [TestCase(0, 1, 1)]
+        [TestCase(1, 1, 1)]
+        [TestCase(0, 0, 0)]
+        [TestCase(0.25, 0.25, 0.4375)]
+        [TestCase(0.3, 0.9, 0.93)]
+        [TestCase(0.3, 0.3, 0.51)]
+        public void Either_should_return_the_sum_of_the_inputs_minus_their_product(decimal A, decimal B, decimal expected)
+        {
+            // arrange
+            var sut = GetSubject();
+
+            // act
+            var actual = sut.Either(A, B);
+
+            // assert
+            actual.Should().Be(expected);
+        }
+
 
         private static ProbabilityCalculator GetSubject()
         {
