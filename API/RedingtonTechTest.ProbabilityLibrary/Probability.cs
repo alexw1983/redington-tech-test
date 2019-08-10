b4804551a8828018ad4e398d5722fd8acccabdb9
Alex Wilson <alex.wilson@bud.co.uk>
2019-08-10 22:21:25 +0100

    Added more comments to Probability class

--- API/RedingtonTechTest.ProbabilityLibrary/Probability.cs
+++ API/RedingtonTechTest.ProbabilityLibrary/Probability.cs
@@ -2,19 +2,35 @@
 
 namespace RedingtonTechTest.ProbabilityLibrary
 {
+    /// <summary>
+    /// Contains a value representing a probability
+    /// </summary>
     public class Probability
     {
-        private static bool _isValid(decimal probability) => probability >= 0 && probability <= 1;
-
         public decimal Value { get; }
 
+        /// <summary>
+        /// Initializes a new instance of the Probability class
+        /// </summary>
+        /// <param name="value">A decimal value that MUST be between 0 and 1</param>
         public Probability(decimal value)
         {
-            if (!_isValid(value))
+            // This should be handled by validation already and is technically duplicated logic
+            // but this class should not rely on some validation occuring elsewhere so 
+            // validating here as well.
+
+            if (!IsValid(value))
                 throw new ArgumentOutOfRangeException(nameof(value), $"Value {value} is not between 0 and 1");
 
             Value = value;
         }
+
+        /// <summary>
+        /// Fails if value is not between 0 and 1
+        /// </summary>
+        /// <param name="probability">Value to be checked</param>
+        /// <returns>False if value is not between 0 and 1</returns>
+        public static bool IsValid(decimal probability) => probability >= 0 && probability <= 1;
 
         /// <summary>
         /// Calculates the probability of A combined with B i.e. P(A)P(B)
